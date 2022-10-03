8146210fe5d77755cb857ff26b1e236a42a0e4e9
vinayaknayar <nayarvinayak03@gmail.com>
2022-10-03 23:21:50 +0530

    changed app name to counter

--- config/prod.exs
+++ config/prod.exs
@@ -47,7 +47,7 @@ config :logger, level: :info
 #       force_ssl: [hsts: true]
 #
 # Check `Plug.SSL` for all available options in `force_ssl`.
-config :whitesmoke-fuchsia-siskin, CounterWeb.Endpoint,
+config :counter, CounterWeb.Endpoint,
   http: [port: {:system, "PORT"}], # Possibly not needed, but doesn't hurt
   url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
