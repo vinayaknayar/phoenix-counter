44a0990cff0f6728a44020d11b06dfa1cb37959d
vinayaknayar <nayarvinayak03@gmail.com>
2022-10-04 12:05:10 +0530

    prod modified-2

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ config :counter, CounterWeb.Endpoint, cache_static_manifest: "priv/static/cache_
 
 config :counter, CounterWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "https://whitesmoke-fuchsia-siskin.gigalixirapp.com", port: 80],
+  url: [host: "localhost", port: 80],
   # cache_static_manifest: "priv/static/cache_manifest.json"
 # Do not print debug messages in production
 config :logger, level: :info
